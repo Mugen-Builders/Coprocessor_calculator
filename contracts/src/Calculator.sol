5e8c3b96be1ce73bd954637d81b49b698f968178
Idogwu Chinonso <idogwuchinonso1@gmail.com>
2025-05-11 13:52:58 +0100

    feat(calculator): implemented proper records and avoids multiple call with same request

--- contracts/src/Calculator.sol
+++ contracts/src/Calculator.sol
@@ -6,8 +6,11 @@ import "coprocessor-adapter-2.3.0/src/CoprocessorAdapter.sol";
 contract Calculator is CoprocessorAdapter {
 
     enum OperationType { Add, Subtract, Divide, Multiply }
+
+    uint256 totalRequest;
+
     mapping (bytes32 => Record) public encodedRequestToResponse;
-    mapping (address => bytes32[]) public userToRequestHistory;
+    mapping (address => History[]) public userToRequestHistory;
 
     struct Operation {
         uint256 firstNumber;
@@ -21,23 +24,52 @@ contract Calculator is CoprocessorAdapter {
         bytes result;
     }
 
+    struct History {
+        uint256 requestId;
+        bytes32 request;
+        uint256 requestTime;
+    }
+
     event EncodedOperation(bytes encoded);
     event receivedResult(bytes result);
+    event ComputationResultAlreadyExists(bytes32 request, bytes result);
 
     constructor(address _taskIssuerAddress, bytes32 _machineHash)
         CoprocessorAdapter(_taskIssuerAddress, _machineHash)
     {}
+
+    function getUserRequests(address user) public view returns (History[] memory) {
+        return userToRequestHistory[user];
+    }
+
+    function getRequestResponse( bytes32 request) public view returns (Record memory) {
+        return encodedRequestToResponse[request];
+    }
 
 
     function handleNotice(bytes32 payloadHash, bytes memory notice) internal override {
         recordResult(payloadHash, notice);
     }
 
-    function recordRequest(bytes32 request) internal {
+    function recordRequest(bytes32 request) internal returns (bool) {
+        totalRequest ++;
         uint id = userToRequestHistory[msg.sender].length;
-        Record memory record = Record(id, request, '0x00');
-        userToRequestHistory[msg.sender].push(request);
-        encodedRequestToResponse[request] = record;
+
+        History memory newHistory = History(id, request, block.timestamp);
+        userToRequestHistory[msg.sender].push(newHistory);
+
+        if (encodedRequestToResponse[request].id == 0) {
+            Record memory record = Record(totalRequest, request, '0x00');
+            encodedRequestToResponse[request] = record;
+            return true;
+        } else {
+            bytes memory result =  encodedRequestToResponse[request].result;
+            Record memory record = Record(totalRequest, request, result);
+            encodedRequestToResponse[request] = record;
+            emit ComputationResultAlreadyExists(request, result);
+            return false;
+        }
+        
     }
 
     function recordResult(bytes32 request, bytes memory response) internal {
@@ -49,36 +81,38 @@ contract Calculator is CoprocessorAdapter {
     function add(uint256 a, uint256 b) external {
         Operation memory op = Operation(a, b, OperationType.Add);
         bytes memory encoded = abi.encode(op);
+        if (recordRequest(keccak256(encoded))) {
+            callCoprocessor(encoded);
+        }
         emit EncodedOperation(encoded);
-        recordRequest(keccak256(encoded));
-        callCoprocessor(encoded);
     }
 
     function subtract(uint256 a, uint256 b) external{
         Operation memory op = Operation(a, b, OperationType.Subtract);
         bytes memory encoded = abi.encode(op);
+        if (recordRequest(keccak256(encoded))) {
+            callCoprocessor(encoded);
+        }
         emit EncodedOperation(encoded);
-        recordRequest(keccak256(encoded));
-        callCoprocessor(encoded);
     }
 
     function divide(uint256 a, uint256 b) external {
         require(b != 0, "Division by zero");
         Operation memory op = Operation(a, b, OperationType.Divide);
         bytes memory encoded = abi.encode(op);
+        if (recordRequest(keccak256(encoded))) {
+            callCoprocessor(encoded);
+        }
         emit EncodedOperation(encoded);
-        recordRequest(keccak256(encoded));
-        callCoprocessor(encoded);
     }
 
-        function multiply(uint256 a, uint256 b) external {
+    function multiply(uint256 a, uint256 b) external {
         Operation memory op = Operation(a, b, OperationType.Multiply);
         bytes memory encoded = abi.encode(op);
+        if (recordRequest(keccak256(encoded))) {
+            callCoprocessor(encoded);
+        }
         emit EncodedOperation(encoded);
-        recordRequest(keccak256(encoded));
-        callCoprocessor(encoded);
     }
-
-
 }
 
